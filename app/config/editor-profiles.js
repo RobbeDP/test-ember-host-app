c2ae71c7d4cfa84b782f1f225a479803aebb6599
abeforgit <arnebertrand@gmail.com>
2021-07-15 12:11:06 +0200

    Fix editor setup

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,10 +1,7 @@
 export default {
-  all: [
-    // This list will contain the plugins you want to run for the 'all' profile.
+  default: [
   ],
-  /*  An additional profile may be added,  e.g.
-     adminUser: [
-      // List of plugins useful for an administrator.
-     ]
-  */
+  all: [
+  ],
+  none: []
 };
